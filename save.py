a0b1507f84cfd305ea34215eed729c0d409531ad
s151003 <s151003@kashiwanoha.ed.jp>
2018-06-24 07:15:34 +0900

    機能追加
    ・ユーザー名をiniから取得
    ・ターボモード

--- save.py
+++ save.py
@@ -3,6 +3,7 @@ from twitter import *
 import json
 import urllib.request
 from tweetsave import api 
+from time import sleep
 
 def auth():
     ini = configparser.ConfigParser()
@@ -24,23 +25,41 @@ def get_timeline(screen_name,max=None):
             return t.statuses.user_timeline(screen_name=screen_name,count="200")
     except:
         print("Twitter API取得制限 15分後に再実行")
+        sleep(60*15)
+        get_timeline(screen_name,max=None)
+
         
-def savetweets(tweets):
+def savetweets(tweets,turbo):
     max=""
     l = len(tweets)
     for i in range(0,l):
         response = api.save(str(tweets[i]['id']))
-        print(response['status'],response['message'])
+        print(response)
         print(tweets[i]['id'])
         if i == l:
             max=tweets[i]['id']
+        if turbo != False:
+            sleep(0.9)
     return max
 
-def main():
-    tweets=get_timeline("muratamika2020")
-    max = savetweets(tweets)
-    for i in range(0,4):
-        tweets= get_timeline("muratamika2020",max)
-        max = savetweets(tweets)
+def main():    
+    ini = configparser.ConfigParser()
+    ini.read('./keys.ini','UTF-8')
+    screen_name = ini.get('save','user')
+    turbo=ini.get('save','turbo')
+    print("-----------------------")
+    print("ユーザ名:",screen_name)
+    if turbo:
+        print("ターボモード:オン")
+    else:
+        print("ターボモード:オフ")
+    print("-----------------------")
+    tweets=get_timeline(screen_name)
+    max = savetweets(tweets,turbo)
+    for i in range(0,10):
+        tweets= get_timeline(screen_name,max)
+        max = savetweets(tweets,turbo)
 
 main()
+
+
